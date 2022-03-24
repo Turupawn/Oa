e3e951bf29631b6c5196695539bfed754afe5981
turupawn <ahmed.hn.43@gmail.com>
2022-03-24 09:03:58 -0600

    ex

---
+++ blockchain_stuff.js
@@ -0,0 +1,139 @@
+const NETWORK_ID = 4
+
+const MY_CONTRACT_ADDRESS = "0x26b7CdF3718fb3a89204017Ff34b69FF07Fe9d1E"
+const MY_CONTRACT_ABI_PATH = "./json_abi/MyContract.json"
+var my_contract
+
+var accounts
+var web3
+
+function metamaskReloadCallback() {
+  window.ethereum.on('accountsChanged', (accounts) => {
+    document.getElementById("web3_message").textContent="Se cambió el account, refrescando...";
+    window.location.reload()
+  })
+  window.ethereum.on('networkChanged', (accounts) => {
+    document.getElementById("web3_message").textContent="Se el network, refrescando...";
+    window.location.reload()
+  })
+}
+
+const getWeb3 = async () => {
+  return new Promise((resolve, reject) => {
+    if(document.readyState=="complete")
+    {
+      if (window.ethereum) {
+        const web3 = new Web3(window.ethereum)
+        window.location.reload()
+        resolve(web3)
+      } else {
+        reject("must install MetaMask")
+        document.getElementById("web3_message").textContent="Error: Porfavor conéctate a Metamask";
+      }
+    }else
+    {
+      window.addEventListener("load", async () => {
+        if (window.ethereum) {
+          const web3 = new Web3(window.ethereum)
+          resolve(web3)
+        } else {
+          reject("must install MetaMask")
+          document.getElementById("web3_message").textContent="Error: Please install Metamask";
+        }
+      });
+    }
+  });
+};
+
+const getContract = async (web3, address, abi_path) => {
+  const response = await fetch(abi_path);
+  const data = await response.json();
+  
+  const netId = await web3.eth.net.getId();
+  contract = new web3.eth.Contract(
+    data,
+    address
+    );
+  return contract
+}
+
+async function loadDapp() {
+  metamaskReloadCallback()
+  document.getElementById("web3_message").textContent="Please connect to Metamask"
+  var awaitWeb3 = async function () {
+    web3 = await getWeb3()
+    web3.eth.net.getId((err, netId) => {
+      if (netId == NETWORK_ID) {
+        var awaitContract = async function () {
+          my_contract = await getContract(web3, MY_CONTRACT_ADDRESS, MY_CONTRACT_ABI_PATH)
+          document.getElementById("web3_message").textContent="You are connected to Metamask"
+          onContractInitCallback()
+          web3.eth.getAccounts(function(err, _accounts){
+            accounts = _accounts
+            if (err != null)
+            {
+              console.error("An error occurred: "+err)
+            } else if (accounts.length > 0)
+            {
+              onWalletConnected()
+              document.getElementById("account_address").style.display = "block"
+            } else
+            {
+              document.getElementById("connect_button").style.display = "block"
+            }
+          });
+        };
+        awaitContract();
+      } else {
+        document.getElementById("web3_message").textContent="Please connect to Rinkeby";
+      }
+    });
+  };
+  awaitWeb3();
+}
+
+async function connectWallet() {
+  await window.ethereum.request({ method: "eth_requestAccounts" })
+  accounts = await web3.eth.getAccounts()
+  onWalletConnected()
+}
+
+loadDapp()
+
+const onContractInitCallback = async () => {
+  var shill_group_head = await my_contract.methods.shill_group_head().call()
+  var current_group_str = shill_group_head
+  for(var i=0; i<3; i++)
+  {
+    var current_group = await my_contract.methods.shill_groups(current_group_str).call()
+    console.log("Rank #" + (i+1) + ": " + current_group_str + " " + current_group.cumulative_amount)
+    var current_participant_str = current_group.participant_head
+    for(var j=0; j<3; j++)
+    {
+      var current_participant = await my_contract.methods.participants(current_group_str, current_participant_str).call()
+      if(current_participant.cumulative_amount>0)
+        console.log("  " + current_participant_str + " " + current_participant.cumulative_amount)
+      current_participant_str = current_participant.next
+    }
+    current_group_str = current_group.next
+  }
+}
+
+const onWalletConnected = async () => {
+}
+
+
+//// Minter ////
+
+const minterClaim = async (token_id) => {
+  const result = await minter_contract.methods.claim(token_id)
+  .send({ from: accounts[0], gas: 0, value: 0 })
+  .on('transactionHash', function(hash){
+    document.getElementById("web3_message").textContent="Claiming...";
+  })
+  .on('receipt', function(receipt){
+    document.getElementById("web3_message").textContent="Success.";    })
+  .catch((revertReason) => {
+    console.log("ERROR! Transaction reverted: " + revertReason.receipt.transactionHash)
+  });
+}
